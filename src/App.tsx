3b4d74fd5277eca79e90f6c7d6f31ab3d4e11436
aiichiro <aiichirookuda@gmail.com>
2021-05-18 18:19:22 +0900

    set: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Search } from './components/pages/Search';
+import { Top } from './components/pages/Top';
+import { Watch } from './components/pages/Watch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Top} />
+        <Route path="/search" component={Search} />
+        <Route path="/watch" component={Watch} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
